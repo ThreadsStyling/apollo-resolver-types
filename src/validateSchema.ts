6ab06a9207f3fafb46a168bb4fc5189421844d57
Forbes Lindesay <forbes.lindesay@threadsstyling.com>
2019-10-23 13:03:18 +0100

    test: add test cases for validateSchema

--- src/validateSchema.ts
+++ src/validateSchema.ts
@@ -1,5 +1,7 @@
 import {readFileSync} from 'fs';
-import {parse} from 'graphql/language';
+import {relative} from 'path';
+import {GraphQLError} from 'graphql/error';
+import {parse, DocumentNode} from 'graphql/language';
 import {validateSDL} from 'graphql/validation/validate';
 import {codeFrameColumns} from '@babel/code-frame';
 
@@ -15,13 +17,20 @@ export default function validateSchema(filename: string, isFederated: boolean): 
     schemaString = readFileSync(filename, 'utf8');
   } catch (ex) {
     if (ex.code === 'ENOENT') {
-      console.error(`Could not find the schema at ${filename}`);
+      console.error(`Could not find the schema at ${relative(process.cwd(), filename)}`);
       return process.exit(1);
     }
     throw ex;
   }
 
-  const parsedSchema = parse(isFederated ? federationSpec + schemaString : schemaString);
+  let parsedSchema: DocumentNode;
+  try {
+    parsedSchema = parse(isFederated ? federationSpec + schemaString : schemaString);
+  } catch (ex) {
+    console.error(`GraphQL syntax error in ${relative(process.cwd(), filename)}:\n`);
+    printError(ex, schemaString, isFederated);
+    return process.exit(1);
+  }
 
   if (isFederated) {
     // If we are looking at a federated schema, we may be extending
@@ -39,37 +48,34 @@ export default function validateSchema(filename: string, isFederated: boolean): 
 
   const errors = validateSDL(parsedSchema);
   if (errors.length) {
-    console.error(`GraphQL schema ${errors.length > 1 ? 'errors' : 'error'} in ${filename}:\n`);
+    console.error(
+      `GraphQL schema ${errors.length > 1 ? 'errors' : 'error'} in ${relative(process.cwd(), filename)}:\n`,
+    );
     for (const e of errors) {
-      if (e.locations && e.locations.length === 1) {
-        const [loc] = e.locations;
-        console.error(e.message);
-        console.error(
-          '\n' +
-            codeFrameColumns(schemaString, {
-              start: {line: loc.line - (isFederated ? federationSpec.split('\n').length - 1 : 0), column: loc.column},
-            }) +
-            '\n',
-        );
-      } else {
-        console.error(e.message);
-      }
+      printError(e, schemaString, isFederated);
     }
-    process.exit(1);
+    return process.exit(1);
   }
-  // try {
-  //   const errors = validateSDL(parsedSchema);
-  //   if (errors.length) {
-  //     console.log(errors);
-  //     process.exit(1);
-  //   }
-  //   const schema = federation ? federation.buildFederatedSchema(parsedSchema) : buildASTSchema(parsedSchema);
-
-  //   console.log(schema);
-  // } catch (ex) {
-  //   console.dir(Object.keys(ex));
-  //   throw ex;
-  // }
 
   return {source: schemaString};
+}
+
+function printError(
+  e: GraphQLError | {message: string; locations: undefined},
+  schemaString: string,
+  isFederated: boolean,
+) {
+  if (e.locations && e.locations.length === 1) {
+    const [loc] = e.locations;
+    console.error(e.message);
+    console.error(
+      '\n' +
+        codeFrameColumns(schemaString, {
+          start: {line: loc.line - (isFederated ? federationSpec.split('\n').length - 1 : 0), column: loc.column},
+        }) +
+        '\n',
+    );
+  } else {
+    console.error(e.message);
+  }
 }
