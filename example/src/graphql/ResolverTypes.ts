bf6b88eb96ce624f4f21d234ba5a913a6cdfa5a5
Forbes Lindesay <forbes.lindesay@threadsstyling.com>
2020-03-03 09:42:03 +0000

    fix: duplicated imports in new TypeScript version

--- example/src/graphql/ResolverTypes.ts
+++ example/src/graphql/ResolverTypes.ts
@@ -1,6 +1,6 @@
 import ApolloResolverTypes from '../../../'; // in a real project this is `from '@threads/apollo-resolver-types';`
 import * as GeneratedTypes from './__generated__/types';
 
-type ResolverTypes = ApolloResolverTypes<GeneratedTypes.Resolvers>;
+type ResolverTypes = ApolloResolverTypes<GeneratedTypes.IResolvers>;
 
 export default ResolverTypes;
