2fe9d3d267767f784f7026faaded09e24584aef4
Dan Lincoln <dan.lincoln.dev@gmail.com>
2023-07-18 11:16:42 +0100

    chore: Fix resolver types

--- src/types/ExtractObjectResolverConfig.ts
+++ src/types/ExtractObjectResolverConfig.ts
@@ -1,3 +1,4 @@
+import {IResolverObject} from 'graphql-tools';
 import ExtractPropsWithType from './ExtractPropsWithType';
 import MakePropsRequired from './MakePropsRequired';
 
@@ -5,5 +6,8 @@ import MakePropsRequired from './MakePropsRequired';
  * Take the resolvers and extract just the object type resolvers, marking any
  * that are needed as required
  */
-type ExtractObjectResolverConfig<Resolvers> = ExtractPropsWithType<MakePropsRequired<Resolvers>, Record<any, any>>;
+type ExtractObjectResolverConfig<Resolvers> = ExtractPropsWithType<
+  MakePropsRequired<Resolvers>,
+  IResolverObject<any, any>
+>;
 export default ExtractObjectResolverConfig;
